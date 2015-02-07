8d8434f0552b07bbddc8bd29d8e3ebf01097329a
廣野 まゆ子 <mayuko.hirono@gmail.com>
2015-02-07 15:13:09 +0900

    adds threads for each requests

--- currency.rb
+++ currency.rb
@@ -5,8 +5,17 @@ start = Time.now
 currencies =
 ['USD', 'CAD', 'HKD', 'EUR', 'JPY']
 
+threads = []
+
 currencies.each do |currency|
-  p Net::HTTP.get('download.finance.yahoo.com', "/d/quotes.csv?e=.csv&f=sl1d1t1&s=USD#{currency}=X")
+  threads << Thread.new do
+    p Net::HTTP.get('download.finance.yahoo.com', "/d/quotes.csv?e=.csv&f=sl1d1t1&s=USD#{currency}=X")
+  end
 end
 
-p "DONE------- #{start - Time.now} sec" #returns 1.922443 sec ~ 2.041589 sec
+# conbines all threads to main thread
+threads.each do |t|
+  t.join
+end
+
+p "DONE------- #{Time.now - start} sec" #returns 0.393388 sec ~ 0.580347 sec
